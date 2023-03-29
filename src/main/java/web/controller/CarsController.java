54e756d7adf8aa063ccacecf6f5011d8e718078c
hammondy <hammondy@student.21-school.ru>
2023-03-29 14:20:44 +0700

    spring-mvc fix

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarServiceImpl;
 
-import java.util.List;
-
 @Controller
 public class CarsController {
     @GetMapping(value = "/cars")
