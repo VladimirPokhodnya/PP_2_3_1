2ec70a0a8f9acdc765c4efadb597c0de81eb9f17
hammondy <hammondy@student.21-school.ru>
2023-03-29 14:19:45 +0700

    spring-mvc fix

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -4,15 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
 import web.service.CarServiceImpl;
+
 import java.util.List;
 
 @Controller
 public class CarsController {
-
     @GetMapping(value = "/cars")
-    public String printCars(@RequestParam(defaultValue="5") Integer count, ModelMap model) {
+    public String printCars(@RequestParam(defaultValue = "5") Integer count, ModelMap model) {
         model.addAttribute("cars", new CarServiceImpl().getCars(count));
         return "cars";
     }
