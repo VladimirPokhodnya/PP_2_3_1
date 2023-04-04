7a9f778c27db19d0af311878f1152eba48ca484e
hammondy <hammondy@student.21-school.ru>
2023-04-04 08:13:27 +0700

    users

--- src/main/java/web/controller/UsersController.java
+++ src/main/java/web/controller/UsersController.java
@@ -4,20 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarService;
-import web.service.CarServiceImpl;
 
 
 @Controller
-public class CarsController {
+public class UsersController {
     @Autowired
     private CarService carService;
 
-    @GetMapping(value = "/cars")
+    @GetMapping(value = "/users")
     public String printCars(@RequestParam(defaultValue = "5") Integer count, ModelMap model) {
         model.addAttribute("cars", carService.getCars(count));
-        return "cars";
+        return "users";
     }
 }
